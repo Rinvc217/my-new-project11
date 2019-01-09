db9be9f9fbc65b62b89d1960f6d11d5676e2a738
RinHueCodeGym <rinvc217@gmail.com>
2019-01-09 11:54:58 +0700

    thuchanhtoantu

---
+++ rin3.js
@@ -0,0 +1,7 @@
+var width;
+var height;
+var width = parseInt(prompt("enter the width"));
+var height = parseInt(prompt("enter the height"));
+var dientich = width * height;
+document.write("the dientich is"+dientich);
+
